4f394e0561bfbad1de3af44d05b7f38c0a48f84f
Aashu-hacker <aashishbopche357@gmail.com>
2026-05-02 20:24:08 +0530

    Fix CORS issue

--- server/server.js
+++ server/server.js
@@ -15,9 +15,6 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
-
-app.options("*", cors());
-
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
